761f5d5df0d35676c2ba07e1852289d36b53d1b9
George <georuiz817@gmail.com>
2019-10-04 12:33:00 -0400

    Done.

---
+++ index.js
@@ -0,0 +1,11 @@
+function divide(num1 = 2000,num2 = 100){ 
+ return num1 / num2 
+ }
+
+// arrow function for square 
+let square = num => num * num 
+
+//arrow function for add 
+let add = (num1,num2) => num1 + num2 
+
+//why can't all the lessons be easy and informative like this :) 
